784e99bededf02a92d245c7345d638fb2746305f
Ramsey Bland <ramsey.bland@gmail.com>
2018-07-11 21:24:39 -0600

    setup login

--- wwwsrc/src/App.vue
+++ wwwsrc/src/App.vue
@@ -1,22 +1,52 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-dark bg-primary top-header format">
+      <a class="navbar-left">
+        <router-link :to="{ name: 'Home'}" class="navbar-brand">
+          <a>Home</a>
+        </router-link>
+        <router-link :to="{ name: 'Vaults'}" class="navbar-brand">
+          <a>Vaults</a>
+        </router-link>
+      </a>
+      <button type="button" class="navbar-brand btn btn-outline-light" @click="login" v-if="!(user.id)">Sign In/Login</button>
+      <div v-if="user.id" class="d-flex flex-column">
+        <a class="navbar-brand">User: {{user.username}}</a>
+        <button class="navbar-brand btn btn-outline-light" @click="signOut">Sign out</button>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    created() {
+      this.$store.dispatch("authenticate")
+    },
+    computed: {
+      user() {
+        return this.$store.state.user
+      }
+    },
+    methods: {
+      login() {
+        this.$router.push({name: "Login"})
+      },
+      signOut() {
+        this.$store.dispatch("signOut")
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- wwwsrc/src/router/index.js
+++ wwwsrc/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import Vaults from '@/components/Vaults'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/vaults',
+      name: 'Vaults',
+      component: Vaults
     }
   ]
 })
