873bf63c3dd00774a2585d3161c873420a3a163f
Ramsey Bland <ramsey.bland@gmail.com>
2018-07-13 16:13:30 -0600

    redeploy

--- wwwsrc/src/store/index.js
+++ wwwsrc/src/store/index.js
@@ -6,7 +6,7 @@ import userModule from './user';
 import keepsModule from './keeps'
 import vaultModule from './vault'
 var production = !window.location.host.includes('localhost');
-var baseUrl = production ? '//i-de-trippin.herokuapp.com/' : '//localhost:5000/api';
+var baseUrl = production ? '//ramsey-keepr.herokuapp.com/' : '//localhost:5000/api';
 
 vue.use(vuex)
 
